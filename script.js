cdcc93cee22e1c3a2c7f0eba099700d32267b88f
avanich28 <arpawan.v@gmail.com>
2023-07-19 22:18:28 +0700

    set up html and css

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const displayController = (function () {
+  const result = document.querySelector("h1");
+  const radioBtn = document.querySelector(".radio--mark");
+  const btnRestart = document.querySelector(".btn--restart");
+})();
+
+const gameBoard = new Array(9);
+console.log(gameBoard);
+
+// gameBoard, displayController - module
+// player - factory
+
+// game-name: Tic tAc toE
+// game-over: GAME OVER
+// win: WIN
+// draw: DRAW
+// .try-again: TRY AGAIN

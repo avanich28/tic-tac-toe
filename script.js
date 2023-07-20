228edbac70e344fa25312f2fe85f9873f9af2a3a
avanich28 <arpawan.v@gmail.com>
2023-07-20 20:44:00 +0700

    add checkWinner and displayResult

--- script.js
+++ script.js
@@ -18,9 +18,12 @@ let gameBoard = new Array(9).fill("");
 let result;
 const MAX = 9;
 const SEC = 0.15;
+const LINES = 3;
+const DIAGONAL = 2;
+const GAME = "Tic tAc toE";
 
 function Person(mark) {
-  const _score = [0, 0];
+  // const _score = [0, 0];
   const choice = function (e, click = true) {
     const target = !click ? e : e.target;
     if (target.textContent) return;
@@ -30,7 +33,6 @@ function Person(mark) {
 
     displayMark(target, this.mark, index);
   };
-  // check winner func
   return { mark, choice };
 }
 
@@ -49,23 +51,42 @@ const convertToUpperCase = function (str) {
   return str.replace("-", " ").toUpperCase();
 };
 
-const clearGame = function (result = false) {
-  if (result) {
-    resultEl.className = "game-name";
-    resultEl.textContent = "Tic tAc toE";
-  }
-
-  // BUG
-  gameBoard = gameBoard.map((mark) => (mark = ""));
-  Array.from(boxes).forEach((box) => (box.innerHTML = ""));
-};
-
 const getRandomNum = function () {
   const choice = Math.trunc(Math.random() * MAX);
   // BUG
   if (gameBoard[choice]) return getRandomNum();
   else return choice;
 };
+
+const checkMark = function (arr, mark) {
+  return arr.every((x) => x === mark);
+};
+
+const checkWinner = function (gameBoard, mark) {
+  // Check row
+  for (let i = 0; i < LINES; i++) {
+    const time = i * 3;
+    const arr = gameBoard.slice(0 + time, 3 + time);
+    if (checkMark(arr, mark)) return true;
+  }
+
+  // Check column
+  for (let i = 0; i < LINES; i++) {
+    const arr = gameBoard.filter((_, index) => index % 3 === i);
+    if (checkMark(arr, mark)) return true;
+  }
+
+  // Check diagonal
+  for (let i = 0; i < DIAGONAL; i++) {
+    const time = 2 * i + 2;
+    let arr = gameBoard.filter((_, index) => index % time === 0);
+    if (i === 0) arr = arr.slice(1, -1);
+    if (checkMark(arr, mark)) return true;
+  }
+
+  return false;
+};
+console.log(checkWinner(["o", "x", "x", "x", "x", "o", "o", "o", "o"], "o"));
 
 // View
 const displayMark = function (target, mark, i) {
@@ -75,9 +96,28 @@ const displayMark = function (target, mark, i) {
   gameBoard[i] = mark;
 };
 
-const displayResult = function (result) {
-  resultEl.classList = result;
-  resultEl.textContent = convertToUpperCase(result);
+const displayResult = function (strResult) {
+  resultEl.classList.add(strResult);
+  resultEl.textContent = convertToUpperCase(strResult);
+  btnRestart.classList.toggle("try-again");
+  btnRestart.textContent = convertToUpperCase("try-again");
+  Array.from(boxes).forEach((box) =>
+    box.removeEventListener("click", controlPlayer)
+  );
+};
+
+const clearGame = function (result) {
+  resultEl.className = "game-name";
+  resultEl.textContent = GAME;
+  btnRestart.classList.remove("try-again");
+  btnRestart.textContent = convertToUpperCase("restart");
+  Array.from(boxes).forEach((box) =>
+    box.addEventListener("click", controlPlayer)
+  );
+
+  // BUG
+  gameBoard = gameBoard.map((mark) => (mark = ""));
+  Array.from(boxes).forEach((box) => (box.innerHTML = ""));
 };
 
 // Controller
@@ -93,6 +133,9 @@ const controlDisplay = function (e) {
   computer.mark = computerM;
 
   // Re-set game
+  Array.from(boxes).forEach((box) =>
+    box.removeEventListener("click", controlPlayer)
+  );
   clearGame();
 
   // If player's mark is O, computer's X mark starts first.
@@ -102,18 +145,19 @@ const controlDisplay = function (e) {
 
 const controlPlayer = function (e) {
   player.choice.bind(player, e, true)();
+  if (checkWinner(gameBoard, player.mark)) return displayResult("win");
 
-  if (gameBoard.every((mark) => mark !== "")) return;
+  if (gameBoard.every((mark) => mark !== "")) return displayResult("draw");
 
-  setTimeout(
-    () => computer.choice.bind(computer, boxes[getRandomNum()], false)(),
-    SEC * 1000
-  );
+  setTimeout(() => {
+    computer.choice.bind(computer, boxes[getRandomNum()], false)();
+    if (checkWinner(gameBoard, computer.mark)) return displayResult("lose");
+  }, SEC * 1000);
 };
 
 // Start
 const init = function () {
-  // Player plays game
+  // Render mark
   Array.from(boxes).forEach((box) =>
     box.addEventListener("click", controlPlayer)
   );

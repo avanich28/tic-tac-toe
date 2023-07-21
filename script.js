19870376dbc5c3241a85e3740eee7aa505fd0c78
avanich28 <arpawan.v@gmail.com>
2023-07-21 20:04:33 +0700

    store variable as private property

--- script.js
+++ script.js
@@ -1,65 +1,70 @@
 "use strict";
 
 function Player(mark) {
-  const getMark = () => mark;
+  const _mark = mark;
+  const getMark = () => _mark;
 
-  const choice = function (el) {
-    const target = el;
-    if (target.textContent) return;
+  const choice = function (index) {
+    model.checkFill(index);
 
-    const index = +target.dataset.box;
-    model.gameBoard[index] = this.getMark();
+    model.updateGameBoard(index, _mark);
 
-    view.renderMark(target, this.getMark(), index);
+    view.renderMark(index, _mark);
   };
   return { getMark, choice };
 }
 
 const model = (function () {
+  const _gameBoard = new Array(9).fill("");
   const _MAX = 9;
   const _LINES = 3;
   const _DIAGONAL = 2;
-  const gameBoard = new Array(9).fill("");
-  let player;
-  let computer;
 
   const _checkMark = function (arr, mark) {
     return arr.every((x) => x === mark);
   };
 
-  const checkTurn = function (curBoard) {
-    return curBoard.every((mark) => mark !== "");
+  const checkFill = function (i) {
+    return _gameBoard[i] !== "" ? false : true;
+  };
+
+  const updateGameBoard = (i, mark) => {
+    _gameBoard[i] = mark;
+  };
+
+  const checkTurn = function () {
+    return _gameBoard.every((mark) => mark !== "");
   };
 
   const convertToUpperCase = function (str) {
     return str.replace("-", " ").toUpperCase();
   };
 
-  const getRandomNum = function (curBoard) {
+  const getRandomNum = function () {
     const choice = Math.trunc(Math.random() * _MAX);
     // Recursive
-    if (curBoard[choice]) return getRandomNum(curBoard);
+    if (_gameBoard[choice]) return getRandomNum();
     else return choice;
   };
 
-  const checkWinner = function (mark, curBoard) {
+  const checkWinner = function (mark) {
     // Check row
     for (let i = 0; i < _LINES; i++) {
       const time = i * 3;
-      const arr = curBoard.slice(0 + time, 3 + time);
+      const arr = _gameBoard.slice(0 + time, 3 + time);
       if (_checkMark(arr, mark)) return true;
     }
 
     // Check column
     for (let i = 0; i < _LINES; i++) {
-      const arr = curBoard.filter((_, index) => index % 3 === i);
+      const arr = _gameBoard.filter((_, index) => index % 3 === i);
       if (_checkMark(arr, mark)) return true;
     }
 
     // Check diagonal
     for (let i = 0; i < _DIAGONAL; i++) {
       const time = 2 * i + 2;
-      let arr = curBoard.filter((_, index) => index % time === 0);
+      let arr = _gameBoard.filter((_, index) => index % time === 0);
       if (i === 0) arr = arr.slice(1, -1);
       if (_checkMark(arr, mark)) return true;
     }
@@ -67,36 +72,27 @@ const model = (function () {
     return false;
   };
 
-  // Prevent player to click on the board game
-  const removeClickEvent = function (el, func) {
-    el.removeEventListener("click", func);
-  };
-
-  // Add click event back
-  const addClickEvent = function (el, func) {
-    el.addEventListener("click", func);
-  };
-
   return {
-    player,
-    computer,
-    gameBoard,
+    checkFill,
     checkTurn,
+    updateGameBoard,
     convertToUpperCase,
     getRandomNum,
     checkWinner,
-    removeClickEvent,
-    addClickEvent,
   };
 })();
 
 const view = (function () {
   const _resultEl = document.querySelector("h1");
-  const _btnMark = document.querySelector(".radio--mark"); // init
+  const _btnMark = document.querySelector(".radio--mark");
   const _btnRestart = document.querySelector(".btn--restart");
-  const board = document.querySelector(".board");
-  const boxes = document.querySelectorAll(".box");
+  const _boxes = document.querySelectorAll(".box");
+  const _board = document.querySelector(".board");
   const GAME = "Tic tAc toE";
+
+  _btnRestart.addEventListener("click", () =>
+    _btnMark.querySelector(".label-x").click()
+  );
 
   const addHandlerGame = function (handler) {
     _btnMark.addEventListener("click", function (e) {
@@ -108,19 +104,15 @@ const view = (function () {
     });
   };
 
-  const addHandlerPlayerMove = function (handler) {
-    board.addEventListener("click", handler);
+  const addHandlerHumanMove = function (handler) {
+    _board.addEventListener("click", handler);
   };
 
-  const addHandlerRestart = function (handler) {
-    _btnRestart.addEventListener("click", handler);
-  };
-
-  const renderMark = function (target, mark) {
+  const renderMark = function (i, mark) {
     const markup = `<span class="${mark}">${model.convertToUpperCase(
       mark
     )}</span>`;
-    target.innerHTML = markup;
+    _boxes[i].innerHTML = markup;
   };
 
   const renderResult = function (strResult) {
@@ -136,97 +128,89 @@ const view = (function () {
     _btnRestart.classList.remove("try-again");
     _btnRestart.textContent = model.convertToUpperCase("restart");
 
-    board.addEventListener("click", func);
+    _board.addEventListener("click", func);
 
-    model.gameBoard = model.gameBoard.map((mark) => (mark = ""));
-    Array.from(boxes).forEach((box) => (box.innerHTML = ""));
+    for (let i = 0; i < _boxes.length; i++) {
+      model.updateGameBoard(i, "");
+    }
+
+    Array.from(_boxes).forEach((box) => (box.innerHTML = ""));
+  };
+
+  // Prevent player to click on the board game
+  const removeClickEvent = function (func) {
+    _board.removeEventListener("click", func);
   };
 
   return {
-    boxes,
-    board,
     addHandlerGame,
-    addHandlerPlayerMove,
-    addHandlerRestart,
+    addHandlerHumanMove,
     renderMark,
     renderResult,
     clearDisplay,
+    removeClickEvent,
   };
 })();
 
 const controller = (function () {
-  const controlGame = function (mark = "x") {
-    model.player = Player(mark);
-    model.computer = Player(mark === "x" ? "o" : "x");
-
-    // Reset game
-    model.removeClickEvent(view.board, game.playerMove);
-    view.clearDisplay(game.playerMove);
-
-    // If player's mark is O, computer's X mark starts first.
-    if (model.player.getMark() === "o")
-      model.computer.choice.bind(
-        model.computer,
-        view.boxes[model.getRandomNum(model.gameBoard)],
-        false
-      )();
-  };
-
-  const controlResult = function (str, el, func) {
-    view.renderResult(str);
-    model.removeClickEvent(el, func);
-  };
-
-  return { controlGame, controlResult };
-})();
-
-const game = (function () {
+  let _human;
+  let _computer;
   const _SEC = 0.15;
+
+  const _controlResult = function (str, func) {
+    view.renderResult(str);
+    view.removeClickEvent(func);
+  };
 
   const _computerMove = function () {
     setTimeout(() => {
-      model.computer.choice.bind(
-        model.computer,
-        view.boxes[model.getRandomNum(model.gameBoard)]
-      )();
-      if (model.checkWinner(model.computer.getMark(), model.gameBoard))
-        return controller.controlResult("lose", view.board, playerMove);
+      _computer.choice.bind(_computer, model.getRandomNum())();
 
-      if (model.checkTurn(model.gameBoard))
-        return controller.controlResult("draw", view.board, playerMove);
+      if (model.checkWinner(_computer.getMark()))
+        return _controlResult("lose", humanMove);
+
+      if (model.checkTurn()) return _controlResult("draw", humanMove);
     }, _SEC * 1000);
   };
 
-  const playerMove = function (e) {
-    // BUG receives argument 2 times if we use different name function when we remove/add EventListener(el, pointEvent obj)
+  const controlGameStart = function (mark = "x") {
+    _human = Player(mark);
+    _computer = Player(mark === "x" ? "o" : "x");
+
+    // Reset game
+    view.removeClickEvent(humanMove);
+    view.clearDisplay(humanMove);
+
+    if (_human.getMark() === "o")
+      _computer.choice.bind(_computer, model.getRandomNum(), false)();
+  };
+
+  const humanMove = function (e) {
+    // NOTE If we use different name function when we remove/add EventListener, receives argument 2 times(el, pointEvent obj).
     const box = e.target.closest(".box");
     if (!box) return;
 
-    // Player play
-    model.player.choice.bind(model.player, box)();
-    if (model.checkWinner(model.player.getMark(), model.gameBoard))
-      return controller.controlResult("win", view.board, playerMove);
+    _human.choice.bind(_human, +box.dataset.box)();
 
-    if (model.checkTurn(model.gameBoard))
-      return controller.controlResult("draw", view.board, playerMove);
+    if (model.checkWinner(_human.getMark()))
+      return _controlResult("win", humanMove);
+
+    if (model.checkTurn()) return _controlResult("draw", humanMove);
 
     _computerMove();
   };
 
-  return { playerMove };
+  return { controlGameStart, humanMove };
 })();
 
 const init = function () {
   // Render mark when click on board
-  view.addHandlerPlayerMove(game.playerMove);
+  view.addHandlerHumanMove(controller.humanMove);
 
   // Select mark & Restart
-  view.addHandlerGame(controller.controlGame);
-
-  // Restart only
-  view.addHandlerRestart(controller.controlGame);
+  view.addHandlerGame(controller.controlGameStart);
 
   // Default Start
-  controller.controlGame();
+  controller.controlGameStart();
 };
 init();
